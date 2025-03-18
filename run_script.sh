3a5183dd21c1441b4bf2c44cb9a7bde84ce651ae
Czarek Golecki <cezary.golecki@o2.pl>
2025-03-18 22:03:02 +0100

    update learning rate in run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -1,17 +1,17 @@
-sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.00001
-sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.00002
-sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.00005
 sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.0001
 sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.0002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.0005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.002
 
-sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_8x4096 training.optimizer.lr=0.00001
-sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_8x4096 training.optimizer.lr=0.00002
-sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_8x4096 training.optimizer.lr=0.00005
 sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_8x4096 training.optimizer.lr=0.0001
 sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_8x4096 training.optimizer.lr=0.0002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_8x4096 training.optimizer.lr=0.0005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_8x4096 training.optimizer.lr=0.001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_cosformer_8x4096 training.optimizer.lr=0.002
 
-sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_vanilla_8x4096 training.optimizer.lr=0.00001
-sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_vanilla_8x4096 training.optimizer.lr=0.00002
-sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_vanilla_8x4096 training.optimizer.lr=0.00005
 sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_vanilla_8x4096 training.optimizer.lr=0.0001
 sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_vanilla_8x4096 training.optimizer.lr=0.0002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_vanilla_8x4096 training.optimizer.lr=0.0005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_vanilla_8x4096 training.optimizer.lr=0.001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=enwik9_vanilla_8x4096 training.optimizer.lr=0.002
