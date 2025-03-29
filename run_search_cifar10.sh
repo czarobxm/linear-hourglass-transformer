bd7864370e363e9347d6b62bf0fe48880f134755
Czarek Golecki <cezary.golecki@o2.pl>
2025-03-29 16:43:01 +0100

    add run search script for cifar10

---
+++ run_search_cifar10.sh
@@ -0,0 +1,91 @@
+# 8 layers
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.00001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.00002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.00005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.0001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.0002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.0005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,4x2048,2x4096 training.optimizer.lr=0.002
+
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_8x4096 training.optimizer.lr=0.00001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_8x4096 training.optimizer.lr=0.00002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_8x4096 training.optimizer.lr=0.00005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_8x4096 training.optimizer.lr=0.0001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_8x4096 training.optimizer.lr=0.0002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_8x4096 training.optimizer.lr=0.0005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_8x4096 training.optimizer.lr=0.001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_8x4096 training.optimizer.lr=0.002
+
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_8x4096 training.optimizer.lr=0.00001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_8x4096 training.optimizer.lr=0.00002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_8x4096 training.optimizer.lr=0.00005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_8x4096 training.optimizer.lr=0.0001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_8x4096 training.optimizer.lr=0.0002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_8x4096 training.optimizer.lr=0.0005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_8x4096 training.optimizer.lr=0.001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_8x4096 training.optimizer.lr=0.002
+
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,4x2048,2x4096 training.optimizer.lr=0.00001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,4x2048,2x4096 training.optimizer.lr=0.00002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,4x2048,2x4096 training.optimizer.lr=0.00005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,4x2048,2x4096 training.optimizer.lr=0.0001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,4x2048,2x4096 training.optimizer.lr=0.0002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,4x2048,2x4096 training.optimizer.lr=0.0005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,4x2048,2x4096 training.optimizer.lr=0.001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,4x2048,2x4096 training.optimizer.lr=0.002
+
+# 6 layers
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,2x2048,2x4096 training.optimizer.lr=0.00001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,2x2048,2x4096 training.optimizer.lr=0.00002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,2x2048,2x4096 training.optimizer.lr=0.00005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,2x2048,2x4096 training.optimizer.lr=0.0001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,2x2048,2x4096 training.optimizer.lr=0.0002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,2x2048,2x4096 training.optimizer.lr=0.0005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,2x2048,2x4096 training.optimizer.lr=0.001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_2x4096,2x2048,2x4096 training.optimizer.lr=0.002
+
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_1x4096,4x2048,1x4096 training.optimizer.lr=0.00001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_1x4096,4x2048,1x4096 training.optimizer.lr=0.00002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_1x4096,4x2048,1x4096 training.optimizer.lr=0.00005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_1x4096,4x2048,1x4096 training.optimizer.lr=0.0001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_1x4096,4x2048,1x4096 training.optimizer.lr=0.0002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_1x4096,4x2048,1x4096 training.optimizer.lr=0.0005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_1x4096,4x2048,1x4096 training.optimizer.lr=0.001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_1x4096,4x2048,1x4096 training.optimizer.lr=0.002
+
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_6x4096 training.optimizer.lr=0.00001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_6x4096 training.optimizer.lr=0.00002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_6x4096 training.optimizer.lr=0.00005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_6x4096 training.optimizer.lr=0.0001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_6x4096 training.optimizer.lr=0.0002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_6x4096 training.optimizer.lr=0.0005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_6x4096 training.optimizer.lr=0.001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_cosformer_6x4096 training.optimizer.lr=0.002
+
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_6x4096 training.optimizer.lr=0.00001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_6x4096 training.optimizer.lr=0.00002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_6x4096 training.optimizer.lr=0.00005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_6x4096 training.optimizer.lr=0.0001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_6x4096 training.optimizer.lr=0.0002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_6x4096 training.optimizer.lr=0.0005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_6x4096 training.optimizer.lr=0.001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_6x4096 training.optimizer.lr=0.002
+
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,2x2048,2x4096 training.optimizer.lr=0.00001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,2x2048,2x4096 training.optimizer.lr=0.00002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,2x2048,2x4096 training.optimizer.lr=0.00005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,2x2048,2x4096 training.optimizer.lr=0.0001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,2x2048,2x4096 training.optimizer.lr=0.0002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,2x2048,2x4096 training.optimizer.lr=0.0005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,2x2048,2x4096 training.optimizer.lr=0.001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_2x4096,2x2048,2x4096 training.optimizer.lr=0.002
+
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_1x4096,4x2048,1x4096 training.optimizer.lr=0.00001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_1x4096,4x2048,1x4096 training.optimizer.lr=0.00002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_1x4096,4x2048,1x4096 training.optimizer.lr=0.00005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_1x4096,4x2048,1x4096 training.optimizer.lr=0.0001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_1x4096,4x2048,1x4096 training.optimizer.lr=0.0002
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_1x4096,4x2048,1x4096 training.optimizer.lr=0.0005
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_1x4096,4x2048,1x4096 training.optimizer.lr=0.001
+sbatch --partition=common --qos=czarekg_common --gres=gpu:1 --time=1-0 poetry run python3 train_single_gpu.py --config-name=cifar10_vanilla_1x4096,4x2048,1x4096 training.optimizer.lr=0.002
