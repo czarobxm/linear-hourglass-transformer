935fe834ee7ebd11f2e084ce9fde45c7d3c2ee30
Czarek Golecki <cezary.golecki@o2.pl>
2025-07-26 16:07:48 +0200

    fix test_time_backward.py

--- test_time_backward.py
+++ test_time_backward.py
@@ -39,8 +39,10 @@ def main(cfg: ExperimentCfg) -> None:
         x = torch.randint(1, tokenizer.vocab_size, (1, seq_len)).to(device)
         inference_times = []
         for _ in range(test_steps):
-            model(x)
-            loss = criterion(model(x), x)
+            print(model(x).shape, x.shape)
+            loss = criterion(
+                model(x).view(seq_len, tokenizer.vocab_size), x.view(seq_len)
+            )
             start.record()
             loss.backward()
             end.record()
